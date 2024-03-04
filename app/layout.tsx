f4b19cc7456e997fbaf975b5dd5818423dcbbee5
Valentino Villar <vles2345@gmail.com>
2024-03-04 10:58:51 -0300

    deploy fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import NavBar from '../components/NavBar'
-import { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'Mayorista el chino',
-}
 
 export default function RootLayout({
   children

afdf1a3332d9a31825130e3ac406b07eb6edeebf
Valentino Villar <vles2345@gmail.com>
2024-03-04 16:21:02 -0300

    deploy fix

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Button, Card, CardBody, CardFooter, CardHeader, Divider } from '@nextui-org/react'
+import { Button, Card, CardFooter, CardHeader, Divider } from '@nextui-org/react'
 import { Edit, Trash } from 'react-feather'
 
 export default function ProductItem() {
@@ -14,18 +14,6 @@ export default function ProductItem() {
                     <p className="text-small text-default-500">CODE:121212</p>
                 </div>
             </CardHeader>
-            <Divider />
-            <CardBody>
-                <div className="flex h-5 items-center space-x-4 text-small">
-                    <div>12$</div>
-                    <Divider orientation="vertical" />
-                    <div>122$</div>
-                    <Divider orientation="vertical" />
-                    <div>123$</div>
-                    <Divider orientation="vertical" />
-                    <div>1232$</div>
-                </div>
-            </CardBody>
             <Divider />
             <CardFooter className='flex gap-3 justify-center items-center'>
                 <Button startContent={<Edit/>}>
--- pages/productos/index.tsx
+++ pages/productos/index.tsx
@@ -1,25 +1,45 @@
-import ProductItem from "@/components/ProductItem"
-import { Button } from "@nextui-org/react"
-import { PlusCircle } from 'react-feather'
+import ProductItem from "@/components/ProductItem";
+import { Button, Tab, Tabs } from "@nextui-org/react";
+import React from "react";
+import { PlusCircle } from 'react-feather';
 
 const Productos = () => {
+  const [selected, setSelected] = React.useState("precio1");
+
+  console.log(selected);
+
+  const handleSelectionChange = (key: string) => {
+    setSelected(key);
+  };
+
   return (
-    <>
-      <div className="flex items-center justify-center flex-col">
-        <div className="pb-4">
-        <Button startContent={<PlusCircle/>} color="success">
+    <div className="flex items-center justify-center flex-col">
+      <div className="pb-4 flex flex-col">
+        <Button startContent={<PlusCircle />} color="success">
           Agregar Producto
         </Button>
-        </div>
-        <div className="gap-2 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-3">
-        <ProductItem/>
-        <ProductItem/>
-        <ProductItem/>
-        <ProductItem/>
+        <div>
+          <Tabs
+            variant='underlined'
+            onSelectionChange={handleSelectionChange}
+            aria-label="Options"
+            selectedKey={selected}
+          >
+            <Tab key="precio1" title="Precio 1" />
+            <Tab key="precio2" title="Precio 2" />
+            <Tab key="precio3" title="Precio 3" />
+            <Tab key="precio4" title="Precio 4" />
+          </Tabs>
         </div>
       </div>
-    </>
-  )
-}
+      <div className="gap-2 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-3">
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+      </div>
+    </div>
+  );
+};
 
-export default Productos
+export default Productos;

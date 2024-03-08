6d88a4691f6e3dc0051019cf7074545af30e109d
vlevillar <vles2345@gmail.com>
2024-03-08 19:16:51 -0300

    modales added

--- components/ClientItem.tsx
+++ components/ClientItem.tsx
@@ -1,31 +1,32 @@
 'use client'
 
-import { Button, Card, CardFooter, CardHeader, Divider } from '@nextui-org/react'
-import { Edit, Trash } from 'react-feather'
+import {
+  Button,
+  Card,
+  CardFooter,
+  CardHeader,
+  Divider
+} from '@nextui-org/react'
+import AccountModal from '@/modals/AccountModal'
 
 export default function ClientItem() {
-
-    return (
-        <Card className="max-w-[300px]">
-            <CardHeader className="flex gap-3 items-center justify-center">
-                <div className="flex flex-col">
-                    <p className="text-md">JUAN</p>
-                    <p className="text-small text-default-500">TEL:12</p>
-                    <p className="text-small text-default-500">Dirección:121212</p>
-                </div>
-            </CardHeader>
-            <Divider />
-            <CardFooter className='flex gap-3 justify-center items-center'>
-                <Button size='sm'>
-                    Editar
-                </Button>
-                <Button color='secondary' size='sm'>
-                    Cuenta corriente
-                </Button>
-                <Button color='danger' size='sm'>
-                    Eliminar
-                </Button>
-            </CardFooter>
-        </Card>
-    )
+  return (
+    <Card className='max-w-[300px]'>
+      <CardHeader className='flex items-center justify-center gap-3'>
+        <div className='flex flex-col'>
+          <p className='text-md'>JUAN</p>
+          <p className='text-small text-default-500'>TEL:12</p>
+          <p className='text-small text-default-500'>Dirección:121212</p>
+        </div>
+      </CardHeader>
+      <Divider />
+      <CardFooter className='flex items-center justify-center gap-3'>
+        <Button size='sm'>Editar</Button>
+        <AccountModal />
+        <Button color='danger' size='sm'>
+          Eliminar
+        </Button>
+      </CardFooter>
+    </Card>
+  )
 }
---
+++ modals/AccountModal.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Checkbox, Input, Link, Tabs, Tab} from "@nextui-org/react";
+import { PlusCircle, Edit, Map, Smartphone, DollarSign } from 'react-feather';
+
+export default function AccountModal() {
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
+  return (
+    <>
+      <Button onPress={onOpen} color="secondary" size="sm">Cuenta corriente</Button>
+      <Modal 
+        isOpen={isOpen} 
+        onOpenChange={onOpenChange}
+        placement="top-center"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Cuenta corriente: JUAN</ModalHeader>
+              <ModalBody className="items-center">
+                <Tabs>
+                    <Tab title="Cuentas"/>
+                    <Tab title="Pago"/>
+                    <Tab title="Saldo"/>
+                </Tabs>
+                <Input
+                  autoFocus
+                  variant="bordered"
+                  endContent={<Edit/>}
+                  startContent={<DollarSign/>}
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="flat" onPress={onClose}>
+                  Cerrar
+                </Button>
+                <Button color="success" onPress={onClose}>
+                  Listo
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}

d54f4a7026f0c5a5b1db88e323b7a4284d228245
Valentino Villar <vles2345@gmail.com>
2024-11-25 04:47:51 -0300

    fix:build

--- components/Order/OrderItem.tsx
+++ components/Order/OrderItem.tsx
@@ -26,16 +26,25 @@ interface Product {
 
 interface Order {
   _id: string
-  clientName: string
   clientId: string
+  clientName: string
   clientNumber: number
-  products: Product[]
+  products: {
+    _id: string
+    name: string
+    prices: number[]
+    quantity: number
+    measurement: string
+  }[]
   discount: string
-  selectedList: number
-  date: string
+  date: string 
   deliveryDate: string
+  documentNumber: number
+  type: string
+  selectedList: number
   description: string
 }
+
 
 interface OrderItemProps {
   order: Order
